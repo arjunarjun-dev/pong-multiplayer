5ceb5714e4369e3a0c67d86249e8511f2b6cc9be
arjunarjun-dev <arj.pg.gupta@gmail.com>
2025-08-03 15:42:00 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
+const socket = io();
+
+// handle receiving player number (0 or 1)
+socket.on('playerNumber', (number) => {
+  console.log('You are Player', number);
+  // assign to left or right paddle
+});
+
+// when moving paddle
+document.addEventListener('keydown', function(e) {
+  // send paddle position to server
+  socket.emit('paddleMove', playerY);
+});
+
+// receive opponent paddle updates
+socket.on('updateOpponent', (opponentY) => {
+  // update opponent paddle position on screen
+});
 
